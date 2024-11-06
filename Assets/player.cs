adb8b0d533c2b94b87e18500d7d9985124de2c38
sk <290548180@qq.com>
2024-11-07 00:40:45 +0800

    20241107

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public float xInput;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxisRaw("Horizontal");
     }
 }
