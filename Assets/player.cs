2cb6e55a8ec07d19680a7c429a7bc1a7eb3e3304
sk <290548180@qq.com>
2024-11-08 00:42:42 +0800

    20241108

--- Assets/player.cs
+++ Assets/player.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    public float xInput;
-    
+    public Rigidbody2D rb;
+    public float moverSpeed;
+    public float jumpForce;
+    private float xInput; // private时 其他脚本不可见不可用
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
+        rb.velocity = new Vector2(5, rb.velocity.y);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(xInput * moverSpeed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space)) rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 }
