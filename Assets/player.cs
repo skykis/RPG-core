5d663d4165570bf7b7ab9a0b57a5b0313c7c8d66
sk <290548180@qq.com>
2024-11-08 23:07:52 +0800

    20241108 - 为Player添加Animator Sprite - 添加静止和移动动画

--- Assets/player.cs
+++ Assets/player.cs
@@ -2,24 +2,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moverSpeed;
-    public float jumpForce;
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    [SerializeField] private float moverSpeed;
+    [SerializeField] private float jumpForce;
+
     private float xInput; // private时 其他脚本不可见不可用
 
     // Start is called before the first frame update
     private void Start()
     {
-        rb.velocity = new Vector2(5, rb.velocity.y);
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        Movement();
+
+        CheckInput();
+
+        AnimatorController();
+    }
+
+    private void CheckInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(xInput * moverSpeed, rb.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space)) rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    private void Movement()
+    {
+        rb.velocity = new Vector2(xInput * moverSpeed, rb.velocity.y);
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void AnimatorController()
+    {
+        anim.SetBool(IsMoving, rb.velocity.x != 0);
     }
 }
