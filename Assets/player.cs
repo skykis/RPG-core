f7186e07885edc222bb6462abaa643c5c5e81a4b
sk <290548180@qq.com>
2024-11-09 18:07:39 +0800

    1.移动时的人物转向 2.跳跃下落的判定及动画

--- Assets/player.cs
+++ Assets/player.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private static readonly int IsMoving = Animator.StringToHash("isMoving");
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
+    private static readonly int YVelocity = Animator.StringToHash("yVelocity");
+    
     private Rigidbody2D rb;
     private Animator anim;
 
@@ -10,6 +13,14 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce;
 
     private float xInput; // private时 其他脚本不可见不可用
+    private int facingDirection = 1;
+    private bool facingRight = true;
+
+    [Header("Collision info")] 
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
 
     // Start is called before the first frame update
     private void Start()
@@ -21,18 +32,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // 移动
         Movement();
-
+        // 输入检测
         CheckInput();
-
+        // 碰撞控制
+        CollisionChecks();
+        // 人物方向控制
+        FlipController();
+        // 动画控制
         AnimatorController();
+    }
+
+    private void CollisionChecks()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void CheckInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
     }
 
     private void Movement()
@@ -47,6 +71,35 @@ public class Player : MonoBehaviour
 
     private void AnimatorController()
     {
-        anim.SetBool(IsMoving, rb.velocity.x != 0);
+        var isMoving = rb.velocity.x != 0;
+
+        anim.SetFloat(YVelocity, rb.velocity.y);
+
+        anim.SetBool(IsMoving, isMoving);
+        anim.SetBool(IsGrounded, isGrounded);
+    }
+
+    private void Flip()
+    {
+        facingDirection = facingDirection * -1;
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void FlipController()
+    {
+        switch (rb.velocity.x)
+        {
+            case > 0 when !facingRight:
+            case < 0 when facingRight:
+                Flip();
+                break;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position,
+            new Vector3(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
