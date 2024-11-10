b2967144cfc216d80ca11d2ee094beac75b983ae
sk <290548180@qq.com>
2024-11-10 15:11:20 +0800

    攻击，连段及对应动画的实现

---
+++ Assets/PlayerAnimEvents.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerAnimEvents : MonoBehaviour
+{
+    private Player player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponentInParent<Player>();
+    }
+
+    private void AnimationTrigger()
+    {
+        player.AttackOver();
+    }
+}
--- Assets/player.cs
+++ Assets/player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
     private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
     private static readonly int YVelocity = Animator.StringToHash("yVelocity");
     private static readonly int IsDashing = Animator.StringToHash("isDashing");
+    private static readonly int IsAttacking = Animator.StringToHash("isAttacking");
+    private static readonly int ComboCounter = Animator.StringToHash("comboCounter");
 
     private Rigidbody2D rb;
     private Animator anim;
@@ -23,6 +25,12 @@ public class Player : MonoBehaviour
     [SerializeField] private float dashCooldown;
     private float dashTime;
     private float dashCooldownTimer;
+
+    [Header("Attack info")]
+    [SerializeField] private float comboTime;
+    private float comboTimeWindow;
+    private bool isAttacking;
+    private int comboCounter;
     
     [Header("Collision info")]
     [SerializeField] private float groundCheckDistance;
@@ -53,6 +61,7 @@ public class Player : MonoBehaviour
 
         dashTime -= Time.deltaTime;
         dashCooldownTimer -= Time.deltaTime;
+        comboTimeWindow -= Time.deltaTime;
     }
 
     private void CollisionChecks()
@@ -73,11 +82,30 @@ public class Player : MonoBehaviour
         {
             DashAbility();
         }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartAttackEvent();
+        }
+    }
+
+    private void StartAttackEvent()
+    {
+        if (!isGrounded)
+        {
+            return;
+        }
+        if (comboTimeWindow < 0)
+        {
+            comboCounter = 0;
+        }
+        isAttacking = true;
+        comboTimeWindow = comboTime;
     }
 
     private void DashAbility()
     {
-        if (dashCooldownTimer < 0)
+        if (dashCooldownTimer < 0 && !isAttacking)
         {
             dashCooldownTimer = dashCooldown;
             dashTime = dashDuration;
@@ -86,9 +114,13 @@ public class Player : MonoBehaviour
 
     private void Movement()
     {
-        if (dashTime > 0)
+        if (isAttacking)
         {
-            rb.velocity = new Vector2(xInput * dashSpeed, 0);
+            rb.velocity = new Vector2(0, 0);
+        }
+        else if (dashTime > 0)
+        {
+            rb.velocity = new Vector2(facingDirection * dashSpeed, 0);
         }
         else
         {
@@ -98,6 +130,10 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
+        if (isAttacking)
+        {
+            return;
+        }
         rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
@@ -110,6 +146,8 @@ public class Player : MonoBehaviour
         anim.SetBool(IsMoving, isMoving);
         anim.SetBool(IsGrounded, isGrounded);
         anim.SetBool(IsDashing, dashTime > 0);
+        anim.SetBool(IsAttacking, isAttacking);
+        anim.SetInteger(ComboCounter, comboCounter);
     }
 
     private void Flip()
@@ -127,6 +165,17 @@ public class Player : MonoBehaviour
             case < 0 when facingRight:
                 Flip();
                 break;
+        }
+    }
+
+    public void AttackOver()
+    {
+        isAttacking = false;
+        
+        comboCounter++;
+        if (comboCounter > 2)
+        {
+            comboCounter = 0;
         }
     }
 }
